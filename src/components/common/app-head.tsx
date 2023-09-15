f020da65759edd03fd83461d79587f72fb22873b
Muhammad Bilal Khalid <64368766+bilalkhalidshaikh@users.noreply.github.com>
2023-09-15 22:02:29 +0500

    first commit

---
+++ src/components/common/app-head.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export function AppHead(): JSX.Element {
+  return (
+    <Head>
+      <title>Degen Diaries</title>
+      <meta name='og:title' content='Degen Diaries' />
+      <link rel='icon' href='/favicon.ico' />
+      <link rel='manifest' href='/site.webmanifest' key='site-manifest' />
+      <meta name='twitter:site' content='@ccrsxx' />
+      <meta name='twitter:card' content='summary_large_image' />
+    </Head>
+  );
+}
---
+++ src/lib/firebase/config.ts
@@ -0,0 +1,26 @@
+const config = {
+  apiKey: "AIzaSyD-ALyD7Xmm5ClCVYeU8OcDW0OVKU66m4w",
+  authDomain: "twitter-web3.firebaseapp.com",
+  projectId: "twitter-web3",
+  storageBucket: "twitter-web3.appspot.com",
+  messagingSenderId: "925953423560",
+  appId: "1:925953423560:web:0c87f77ad6eb876d2d0dfd",
+  measurementId: "G-NK1704HW5G"
+} as const;
+
+type Config = typeof config;
+
+export function getFirebaseConfig(): Config {
+  if (Object.values(config).some((value) => !value))
+    throw new Error('Firebase config is not set or incomplete');
+
+  return config;
+}
+
+// apiKey: "AIzaSyD-ALyD7Xmm5ClCVYeU8OcDW0OVKU66m4w",
+// authDomain: "twitter-web3.firebaseapp.com",
+// projectId: "twitter-web3",
+// storageBucket: "twitter-web3.appspot.com",
+// messagingSenderId: "925953423560",
+// appId: "1:925953423560:web:0c87f77ad6eb876d2d0dfd",
+// measurementId: "G-NK1704HW5G"
