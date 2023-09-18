10ca956a2ffb70c75b43ac5ac86d49e502aa50ad
bilalkhalidshaikh <muhammadbilalkhalid56@gmail.com>
2023-09-19 03:36:20 +0500

    Web3 Auth

--- src/lib/context/web3-auth.tsx
+++ src/lib/context/web3-auth.tsx
@@ -1,27 +1,27 @@
-// web3-auth.tsx
+// // web3-auth.tsx
 
-import Web3 from 'web3';
+// import Web3 from 'web3';
 
-declare global {
-  interface Window {
-    ethereum?: any; // or typeof ethereum
-  }
-}
-const web3Auth = async (): Promise<{ accounts: string[] }> => {
-  // Check if Web3 is injected by MetaMask
-  if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
-    try {
-      // Request account access
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
-      const web3 = new Web3(window.ethereum);
-      const accounts = await web3.eth.getAccounts();
-      return { accounts };
-    } catch (error) {
-      throw new Error('Error connecting to MetaMask.');
-    }
-  } else {
-    throw new Error('MetaMask is not installed.');
-  }
-};
+// declare global {
+//   interface Window {
+//     ethereum?: any; // or typeof ethereum
+//   }
+// }
+// const web3Auth = async (): Promise<{ accounts: string[] }> => {
+//   // Check if Web3 is injected by MetaMask
+//   if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+//     try {
+//       // Request account access
+//       await window.ethereum.request({ method: 'eth_requestAccounts' });
+//       const web3 = new Web3(window.ethereum);
+//       const accounts = await web3.eth.getAccounts();
+//       return { accounts };
+//     } catch (error) {
+//       throw new Error('Error connecting to MetaMask.');
+//     }
+//   } else {
+//     throw new Error('MetaMask is not installed.');
+//   }
+// };
 
-export default web3Auth;
+// export default web3Auth;
--- src/lib/context/web3-context.tsx
+++ src/lib/context/web3-context.tsx
@@ -1,78 +1,78 @@
-import {
-  useEffect,
-  useState,
-  createContext,
-  useContext,
-  ReactNode
-} from 'react';
-import Web3 from 'web3';
-import WalletConnectProvider from '@walletconnect/web3-provider';
+// import {
+//   useEffect,
+//   useState,
+//   createContext,
+//   useContext,
+//   ReactNode
+// } from 'react';
+// import Web3 from 'web3';
+// import WalletConnectProvider from '@walletconnect/web3-provider';
 
-interface Web3ContextProps {
-  web3: Web3 | null;
-  handleWeb3Registration: () => Promise<void>;
-}
+// interface Web3ContextProps {
+//   web3: Web3 | null;
+//   handleWeb3Registration: () => Promise<void>;
+// }
 
-const Web3Context = createContext<Web3ContextProps>({
-  web3: null,
-  handleWeb3Registration: async () => {} // Placeholder function
-});
+// const Web3Context = createContext<Web3ContextProps>({
+//   web3: null,
+//   handleWeb3Registration: async () => {} // Placeholder function
+// });
 
-interface Web3ProviderProps {
-  children: ReactNode;
-}
+// interface Web3ProviderProps {
+//   children: ReactNode;
+// }
 
-const Web3Provider: React.FC<Web3ProviderProps> = ({ children }) => {
-  const [web3, setWeb3] = useState<Web3 | null>(null);
-  const [isConnecting, setIsConnecting] = useState(false);
+// const Web3Provider: React.FC<Web3ProviderProps> = ({ children }) => {
+//   const [web3, setWeb3] = useState<Web3 | null>(null);
+//   const [isConnecting, setIsConnecting] = useState(false);
 
-  const connectWeb3 = async () => {
-    setIsConnecting(true);
+//   const connectWeb3 = async () => {
+//     setIsConnecting(true);
 
-    try {
-      const provider = new WalletConnectProvider({
-        rpc: {
-          1: 'https://mainnet.eth.aragon.network'
-          // Add more networks if needed
-        }
-      });
+//     try {
+//       const provider = new WalletConnectProvider({
+//         rpc: {
+//           1: 'https://mainnet.eth.aragon.network'
+//           // Add more networks if needed
+//         }
+//       });
 
-      await provider.enable();
-      const _web3 = new Web3(provider);
-      setWeb3(_web3);
-    } catch (error) {
-      console.error('Error connecting to WalletConnect:', error.message);
-    } finally {
-      setIsConnecting(false);
-    }
-  };
+//       await provider.enable();
+//       const _web3 = new Web3(provider);
+//       setWeb3(_web3);
+//     } catch (error) {
+//       console.error('Error connecting to WalletConnect:', error.message);
+//     } finally {
+//       setIsConnecting(false);
+//     }
+//   };
 
-  const handleWeb3Registration = async () => {
-    if (!web3) {
-      console.error('Web3 instance is not available.');
-      return;
-    }
+//   const handleWeb3Registration = async () => {
+//     if (!web3) {
+//       console.error('Web3 instance is not available.');
+//       return;
+//     }
 
-    try {
-      const accounts = await web3.eth.getAccounts();
-      console.log('Web3 accounts:', accounts);
-      // Here you can use the accounts obtained from Web3 registration
-    } catch (error) {
-      console.error('Error getting accounts:', error.message);
-    }
-  };
+//     try {
+//       const accounts = await web3.eth.getAccounts();
+//       console.log('Web3 accounts:', accounts);
+//       // Here you can use the accounts obtained from Web3 registration
+//     } catch (error) {
+//       console.error('Error getting accounts:', error.message);
+//     }
+//   };
 
-  return (
-    <Web3Context.Provider
-      value={{ web3, handleWeb3Registration, connectWeb3, isConnecting }}
-    >
-      {children}
-    </Web3Context.Provider>
-  );
-};
+//   return (
+//     <Web3Context.Provider
+//       value={{ web3, handleWeb3Registration, connectWeb3, isConnecting }}
+//     >
+//       {children}
+//     </Web3Context.Provider>
+//   );
+// };
 
-const useWeb3 = (): Web3ContextProps => {
-  return useContext(Web3Context);
-};
+// const useWeb3 = (): Web3ContextProps => {
+//   return useContext(Web3Context);
+// };
 
-export { Web3Provider, useWeb3 };
+// export { Web3Provider, useWeb3 };
