0d8507d7057fba97ea62df98acfeaa872a129460
bilalkhalidshaikh <muhammadbilalkhalid56@gmail.com>
2023-09-20 00:42:44 +0500

    Created Chat & Explore

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: false,
+  images: {
+    domains: ['lh3.googleusercontent.com', 'firebasestorage.googleapis.com']
+  },
   eslint: {
     ignoreDuringBuilds: true
-  },
-  typescript: {
-    ignoreBuildErrors: true // Add this line to ignore TypeScript errors during build
   },
   images: {
     domains: [
